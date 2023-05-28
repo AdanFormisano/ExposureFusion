7ca91721d38f507df32d3e870d5e4803ad09f04a
AdanFormisano <formisano.1813917@studenti.uniroma1.it>
2023-05-28 16:04:42 +0200

    Cleanup

--- exposure_fusion.py
+++ exposure_fusion.py
@@ -1,193 +1,196 @@
-from typing import Any
 import cv2 as cv
 import numpy as np
 
 from dataclasses import dataclass
 
+
 @dataclass
-class Exponents():
+class Exponents:
     contr: float
     sat: float
     exp: float
 
-class ExposureFusion():
-    def __init__(self, mode: str, exp: Exponents = Exponents(1.0, 1.0, 1.0), sigma: float = 0.2, pyramid_layers: int = 4):
+
+class ExposureFusion:
+    def __init__(self, mode: str, n_images: int, exp: Exponents = Exponents(1.0, 1.0, 1.0), sigma: float = 0.2,
+                 pyramid_layers: int = 4):
         # Parameters needed for the exposure fusion
         self.mode: str = mode
+        self.n_images = n_images
         self.sigma: float = sigma
         self.pyramid_layers: int = pyramid_layers
         self.exponents: Exponents = exp
 
         assert pyramid_layers >= 1, "Pyramid levels must be at least 1."
         assert sigma > 0, "Sigma must be positive."
-    
-    
-    def __call__(self, images_original: list[np.ndarray]) -> np.ndarray:
-        """
-        Description
-        """
+        assert n_images > 1, "There must be at least one image."
+
+    def __call__(self, images_original: list[np.ndarray]):
+        # Checks the input images
         try:
             assert len(images_original) >= 3, "Not enough images as input."
-            assert all([image.shape == images_original[0].shape for image in images_original]), "Images in input have different shape."
+            assert all([image.shape == images_original[0].shape for image in
+                        images_original]), "Images in input have different shape."
             assert all([image.shape[-1] == 3 for image in images_original]), "Images in input must be 3-channeled."
         except AssertionError as e:
             print(f"Invalid input: {e}")
-        
+
         # Copy original images
-        images = [image.copy() for image in images_original]        #TODO: Check if really necessary
-        
+        images = [image.copy() for image in images_original]    # CHECK: is it needed to do a copy?
+
         try:
             weights = self.calc_weights(images)
-            for i, img in enumerate(weights):
-                cv.imshow(f"weights {i}", img)
-            
+
             if self.mode == "pyramids":
-                pyramids_gauss, pyramids_lap =  self.build_pyramids(images, weights)
-                pyramid_final = self.blend_pyramids(pyramids_gauss, pyramids_lap, len(images))
+                # Calculates the pyramids need for the multi-resolution fusion
+                pyramids_gauss, pyramids_lap = self.build_pyramids(images, weights)
+                # Build the final laplacian pyramid
+                pyramid_final = self.blend_pyramids(pyramids_gauss, pyramids_lap)
+                # Collapses the final pyramid to create the final hdr image
                 final_image = self.collapse_pyramid(pyramid_final)
-                canvas_laplacian = self.make_canvas(pyramids_gauss[1])
-                
-                
-                return final_image, canvas_laplacian
-            
+
+                # Creates an image with all the pyramid layers represented in it
+                canvas_pyramid = self.make_canvas(pyramids_gauss[1])
+
+                return final_image, canvas_pyramid
+
             elif self.mode == "naive":
-                final_image = self.blend_naive(images, weights)        
+                # Creates the final image using the naive method for the fusion
+                final_image = self.blend_naive(images, weights)
                 return final_image
-            
-        except:
+
+        except Exception:
             raise
-              
-               
-    # Calculates the 3 weights for all the images
+
     def calc_weights(self, images: list[np.ndarray]):
+        """Calculates the weights for all the images."""
+
         weight_sum = np.zeros(images[0].shape[:2], dtype=np.float32)
         weights = []
-        
+
         for image in images:
-            # W = np.ones(image.shape[:2], dtype=np.float32)
             w_contrast = self.calc_weights_contrast(image)
             w_saturation = self.calc_weights_saturation(image)
             w_exposure = self.calc_weights_exposure(image)
-            
-            image_weight = ((w_contrast ** self.exponents.contr) + 1) \
-                        * ((w_saturation ** self.exponents.sat) + 1) \
-                        * ((w_exposure ** self.exponents.exp) + 1)
+
+            image_weight = ((w_contrast ** self.exponents.contr) + 1) *\
+                           ((w_saturation ** self.exponents.sat) + 1) *\
+                           ((w_exposure ** self.exponents.exp) + 1)
+
             weights.append(image_weight)
             weight_sum += image_weight
-            
-        weights = [np.uint8((w / weight_sum)*255) for w in weights]
-        
+
+        # Normalizing the weights values
+        weights = [np.uint8((w / weight_sum) * 255) for w in weights]
+
         return weights
-    
-    
-    # Calculates the contrast metric used for the creation of the weight map
-    def calc_weights_contrast(self, image: np.ndarray):
+
+    def calc_weights_contrast(self, image: np.ndarray) -> np.ndarray:
+        """Calculates the contrast metric used for the creation of the weight map."""
         image_gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-        image_laplacian = cv.Laplacian(image_gray, ddepth=-1)       #TODO: Check why ddepth = -1
+        image_laplacian = cv.Laplacian(image_gray, ddepth=-1)  # TODO: Check why ddepth = -1
         w_contrast = np.absolute(image_laplacian)
-        
+
         return w_contrast
-    
-    
-    # Calculates the saturation metric used for the creation of the weight map
+
     def calc_weights_saturation(self, image: np.ndarray):
+        """Calculates the saturation metric used for the creation of the weight map"""
         w_saturation = image.std(axis=2, dtype=np.float32)
-        
+
         return w_saturation
-    
-    
-    # Calculates the exposure metric used for the creation of the weight map
+
     def calc_weights_exposure(self, image: np.ndarray):
-        w_exposure = np.prod(np.exp(-((image - 0.5)**2) / (2*self.sigma)), axis = 2, dtype=np.float32)
-        
+        """Calculates the exposure metric used for the creation of the weight map"""
+        w_exposure = np.prod(np.exp(-((image - 0.5) ** 2) / (2 * self.sigma)), axis=2, dtype=np.float32)
+
         return w_exposure
-    
-    
+
     def blend_naive(self, images: list[np.ndarray], weights: list[np.ndarray]):
+        """Naive method for exposure blending."""
         final_image = np.zeros(images[0].shape, dtype=np.float32)
-        
-        for image in range(len(images)):
-            final_image[...,0] += weights[image] * images[image][...,0]
-            final_image[...,1] += weights[image] * images[image][...,1]
-            final_image[...,2] += weights[image] * images[image][...,2]
-            
+
+        for image in range(self.n_images):
+            final_image[..., 0] += weights[image] * images[image][..., 0]
+            final_image[..., 1] += weights[image] * images[image][..., 1]
+            final_image[..., 2] += weights[image] * images[image][..., 2]
+
         return final_image.astype(np.uint8)
-    
-    
+
     def build_pyramids(self, images: list[np.ndarray], weights: list[np.ndarray]):
+        """Builds the pyramids needed for the multi-resolution fusion method."""
         pyramids_gauss = []
         pyramids_lap = []
-        
-        for i in range(len(images)):
+
+        for i in range(self.n_images):
             pyramids_gauss.append(self.build_pyramid_gauss(weights[i]))
             pyramids_lap.append(self.build_pyramid_lap(self.build_pyramid_gauss(images[i])))
-        
-        
+
         return pyramids_gauss, pyramids_lap
-    
-    
+
     def build_pyramid_gauss(self, image) -> list[np.ndarray]:
+        """Builds the gaussian pyramid."""
         pyramid_gauss = []
-        
+
         low_image = image.copy()
         pyramid_gauss.append(low_image)
-        
+
         for _ in range(self.pyramid_layers):
             low_image = cv.pyrDown(low_image)
             pyramid_gauss.append(low_image)
-        
+
         return pyramid_gauss
-        
-    
+
     def build_pyramid_lap(self, pyramid_gauss) -> list[np.ndarray]:
-        pyramid_lap = []
-        
-        pyramid_lap.append(pyramid_gauss[-1])
-        
+        """Builds the laplacian pyramid."""
+        pyramid_lap = [pyramid_gauss[-1]]
+
         for i in range(self.pyramid_layers, 0, -1):
-            size = (pyramid_gauss[i-1].shape[1], pyramid_gauss[i-1].shape[0])
+            size = (pyramid_gauss[i - 1].shape[1], pyramid_gauss[i - 1].shape[0])
+
             up_image = cv.pyrUp(pyramid_gauss[i], dstsize=size)
-            laplacian_image = cv.subtract(pyramid_gauss[i-1], up_image)
+            laplacian_image = cv.subtract(pyramid_gauss[i - 1], up_image)
             pyramid_lap.append(laplacian_image)
-        
+
         pyramid_lap.reverse()
-        
+
         return pyramid_lap
-    
-    
-    def blend_pyramids(self, pyramids_gauss, pyramids_lap, number_images) -> list[np.ndarray]:
+
+    def blend_pyramids(self, pyramids_gauss, pyramids_lap) -> list[np.ndarray]:
+        """Blends the gaussian and laplacian pyramids to create a new laplacian pyramid that is then used to create
+        the final image."""
         pyramid_final = []
         for layer in range(self.pyramid_layers + 1):
             layer_sum = np.zeros(pyramids_lap[0][layer].shape, dtype=np.float32)
-            for image in range(number_images):
-                layer_sum[...,0] += pyramids_lap[image][layer][...,0] * pyramids_gauss[image][layer]
-                layer_sum[...,1] += pyramids_lap[image][layer][...,1] * pyramids_gauss[image][layer]
-                layer_sum[...,2] += pyramids_lap[image][layer][...,2] * pyramids_gauss[image][layer]
-            
+            for image in range(self.n_images):
+                layer_sum[..., 0] += pyramids_lap[image][layer][..., 0] * pyramids_gauss[image][layer]
+                layer_sum[..., 1] += pyramids_lap[image][layer][..., 1] * pyramids_gauss[image][layer]
+                layer_sum[..., 2] += pyramids_lap[image][layer][..., 2] * pyramids_gauss[image][layer]
+
             pyramid_final.append(layer_sum)
-            
+
         return pyramid_final
-    
-    
+
     def collapse_pyramid(self, pyramid_final) -> np.ndarray:
+        """Builds the final hdr image collapsing the final pyramid."""
         result_image = pyramid_final[-1].copy()
-        
+
         for layer in range(self.pyramid_layers, 0, -1):
-            size = (pyramid_final[layer-1].shape[1], pyramid_final[layer-1].shape[0])
+            size = (pyramid_final[layer - 1].shape[1], pyramid_final[layer - 1].shape[0])
             up_image = cv.pyrUp(result_image, dstsize=size)
-            result_image = cv.add(up_image, pyramid_final[layer-1], dtype=cv.CV_8UC3)
-        
+            result_image = cv.add(up_image, pyramid_final[layer - 1], dtype=cv.CV_8UC3)
+
         return result_image
-    
+
     def make_canvas(self, images: list[np.ndarray]):
-        max_width = images[0].shape[1]
+        """Builds an image where all the layers of a pyramid are represented in the same canvas one next to the
+        other."""
         max_height = images[0].shape[0]
-        
+
         canvas = images[0].copy()
-        
+
         current_x = 0
         for img in images[1:]:
             canvas[max_height - img.shape[0]:, current_x:current_x + img.shape[1]] = img
             current_x += img.shape[1]
-        
+
         return canvas
--- main.py
+++ main.py
@@ -5,60 +5,71 @@ import numpy as np
 from exposure_fusion import ExposureFusion
 
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', type=str)
+parser.add_argument('run_mode', type=str, help="Selects which mode to use for the exposure fusion: 'naive' or \
+                    'pyramids'")
+parser.add_argument('--no-gui', action="store_true", help="Runs without the GUI")
 args = parser.parse_args()
-mode = args.mode
 
-def open_images(images_dir: str = './images') -> list[np.ndarray]:
+
+def open_images(images_dir: str = './images') -> (int, list[np.ndarray]):
     images = []
-    
-    # Checks on the input
+    n_images = 0
+
+    # Runs checks on the input
     try:
         # Check if the path exists
         if not os.path.exists(images_dir):
             raise NotADirectoryError
+
         # Checks if the directory is empty
         elif len(os.listdir(images_dir)) == 0:
             raise FileNotFoundError(f"Directory {images_dir} is empty")
-        
+
         # Checks if the files are .jpg
         for file in os.listdir(images_dir):
             if os.path.splitext(file)[1] != '.jpg':
                 raise FileNotFoundError(f"File {file} is not a jpg")
-            
+
             # Read image and convert to float32 for processing
-            image = np.float32(cv.imread(images_dir + '/' +file)) / 255.0
+            image = np.float32(cv.imread(f"{images_dir}/{file}")) / 255.0
             images.append(image)
+            n_images += 1
 
     except NotADirectoryError:
         print(f"Input Error: Directory '{images_dir}' doesn't exist.")
     except FileNotFoundError as e:
         print(f"Input Error: {e}")
-        
-    return images
-  
-def main(mode):
-    #TODO: Add path for the images
-    image_float32 = open_images('./images')
-    fusion = ExposureFusion(mode)
+
+    return n_images, images
+
+
+def main(fusion_mode):
+    # TODO: Add path to images
+    n_images, image_float32 = open_images('./images')
+
     try:
-        if mode=="pyramid":
+        # CHECK: ExposureFusion() in try block?
+        fusion = ExposureFusion(fusion_mode, n_images)
+
+        if fusion_mode == "pyramids":
             hdr, canvas = fusion(image_float32)
-            cv.imshow(f"Final HDR image, {mode.upper()}", hdr)
-            cv.imshow(f"Laplacan Pyramids", canvas)
-            cv.imwrite(f"./out/{mode.upper()}.jpg", hdr, [cv.IMWRITE_JPEG_QUALITY, 100])    #TODO: Create better lables for the files
-            cv.imwrite(f"./out/{mode.upper()}_pyramid.jpg", canvas, [cv.IMWRITE_JPEG_QUALITY, 100])
-        elif mode == "naive":
+            cv.imshow(f"Final HDR image, {fusion_mode.upper()}", hdr)
+            cv.imshow(f"Laplacian Pyramids", canvas)
+            cv.imwrite(f"./out/{fusion_mode.upper()}.jpg", hdr,
+                       [cv.IMWRITE_JPEG_QUALITY, 100])  # TODO: Create better labels for the files
+            cv.imwrite(f"./out/{fusion_mode.upper()}_pyramid.jpg", canvas, [cv.IMWRITE_JPEG_QUALITY, 100])
+
+        elif fusion_mode == "naive":
             hdr = fusion(image_float32)
-            cv.imshow(f"Final HDR image, {mode.upper()}", hdr)
-            cv.imwrite(f"./out/{mode.upper()}.jpg", hdr, [cv.IMWRITE_JPEG_QUALITY, 100])
+            cv.imshow(f"Final HDR image, {fusion_mode.upper()}", hdr)
+            cv.imwrite(f"./out/{fusion_mode.upper()}.jpg", hdr, [cv.IMWRITE_JPEG_QUALITY, 100])
+
         else:
             raise ValueError
     except ValueError as e:
-        print(f"{type(e).__name__}: '{mode}' mode doesn't exist. Try between 'naive' or 'pyramids'")
-    
+        print(f"{type(e).__name__}: '{fusion_mode}' mode doesn't exist. Try between 'naive' or 'pyramids'")
 
 
 if __name__ == "__main__":
-    main(mode)
+    main(args.run_mode)
     cv.waitKey(0)
